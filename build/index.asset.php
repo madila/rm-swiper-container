6b57ca982aec8b93eb6026fdee4fbe8256c5b3b7
Ruben Madila <ruben@rigorous-digital.co.uk>
2024-05-27 07:45:35 +0100

    Update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => '6c0e0bb80dea2976d96b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'b8ed524a240299224faa');
