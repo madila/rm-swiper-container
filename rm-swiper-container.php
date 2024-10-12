e75147c2e560c7137944b6339a0c2e562024be4e
Ruben Madila <ruben@rigorous-digital.co.uk>
2024-10-12 09:30:17 +0100

    Update rm-swiper-container.php

--- rm-swiper-container.php
+++ rm-swiper-container.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.4.5
- * Author:            The WordPress Contributors
+ * Author:            Ruben Madila
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       swiper-container
